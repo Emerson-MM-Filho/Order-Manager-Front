7ddd326f29d845abd9ef58d95988367d99a1f858
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-05-12 14:09:15 -0400

    refactor how orders saveds are renders

--- js/renderOrdersSaveds.js
+++ js/renderOrdersSaveds.js
@@ -1,14 +1,9 @@
 const renderOrdersSaveds = () => {
-    const clientsSavedsInLocal = localStorage.getItem('clients')
-    const ordersSavedsInLocal = localStorage.getItem('orders')
-
+    const ordersSavedsInLocal = JSON.parse(localStorage.getItem('orders'))
     
-    if (clientsSavedsInLocal !== null && ordersSavedsInLocal !== null) {
-        const clientsSaveds = clientsSavedsInLocal.split(',')
-        const ordersSaveds = ordersSavedsInLocal.split(',')
-
-        for (let index = 0; index < clientsSaveds.length; index += 1) {
-            createOrderContainer(clientsSaveds[index], ordersSaveds[index])
+    if (ordersSavedsInLocal !== null) {
+        for (let index = 0; index < ordersSavedsInLocal.length; index += 1) {
+            createOrderContainer(ordersSavedsInLocal[index].client, ordersSavedsInLocal[index].orders)
         }
     }   
 }
