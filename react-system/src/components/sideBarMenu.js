415f0613f529847c2ab648bacafb3b2b43292a42
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-05-21 09:29:38 -0400

    import icons and images, create side bar and area logo

---
+++ react-system/src/components/sideBarMenu.js
@@ -0,0 +1,19 @@
+import './sideBarMenu.css'
+import logo from '../images/logo.jpeg'
+
+function sideBarMenu() {
+  return (
+    <div id="sideBarMenu">
+      <div id="logoArea">
+        <img src={logo}/>
+        <h1>Beatriz Dias Confeitaria</h1>
+      </div>
+      <div>
+
+      </div>
+      <div></div>
+    </div>
+  )
+}
+
+export default sideBarMenu
