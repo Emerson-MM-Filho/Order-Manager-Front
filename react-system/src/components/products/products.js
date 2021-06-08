d22e6e508cfec33b48dde6e7f6820c190a220803
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-06-08 10:43:26 -0400

    create header content

--- react-system/src/components/products/products.js
+++ react-system/src/components/products/products.js
@@ -1,8 +1,22 @@
 import SectionTitle from "../sectionTitle";
+import MainButton from "../ui/mainButton";
+import {RiPrinterFill} from "react-icons/ri";
 
 const Products = () => {
   return (
-    <SectionTitle title='Produtos'/>
+    <div>
+      <SectionTitle title='Produtos'/>
+      <div className="internSection">
+        <div className="internSectionHeader">
+          <h4>Todos os Produtos</h4>
+          <div className="newOrderButtonsContainer">
+            <MainButton buttonName='Salvar'/>
+            <MainButton buttonName={<RiPrinterFill className="newOrderButtonIcon"/>}/>
+          </div>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
