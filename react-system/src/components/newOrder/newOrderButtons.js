5a1ab303573bebe1764150bc5989e70451a103f1
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-06-08 10:33:30 -0400

    move and rename

--- react-system/src/components/newOrder/newOrderButtons.js
+++
@@ -1,9 +0,0 @@
-import './newOrderButtons.css'
-
-function newOrderButtons({buttonName}) {
-  return (
-    <button className="newOrderButton">{buttonName}</button>
-  )
-}
-
-export default newOrderButtons
