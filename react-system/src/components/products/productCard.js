6d85eaf67cc04753423101eb3ccbbeda890e38f9
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-06-16 10:33:41 -0400

    remove extra text from product price

--- react-system/src/components/products/productCard.js
+++ react-system/src/components/products/productCard.js
@@ -10,7 +10,7 @@ const ProductCard = ({name, description, price, type, image}) => {
       <div className='cardTextContainer'>
         <h1>{name}</h1>
         <p>{description}</p>
-        <h2>R$ {price},00 / {type}</h2>
+        <h2>R$ {price} / {type}</h2>
       </div>
       <div>
         <button className='productEditButton'>Editar</button>
