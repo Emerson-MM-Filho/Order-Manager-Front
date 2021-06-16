14d3eb357e445734849da2913da0ceeb0262c96d
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-06-16 10:32:35 -0400

    create functions to export and delete product flavors

--- react-system/src/components/products/flavorsTable.js
+++ react-system/src/components/products/flavorsTable.js
@@ -1,34 +1,125 @@
 import { useState } from "react";
 import './flavorsTable.css'
 import { AiFillPlusSquare } from 'react-icons/ai'
+import { BsX } from 'react-icons/bs'
 
 const FlavorsTable = ({inputFunction}) => {
-  const [flavorsCount, setFlavorsCount] = useState([1])
-  const [name, setName] = useState('')
-  const [price, setPrice] = useState(0)
+
+  const flavorId = () => Math.floor((Math.random() * 1000000))
+
+  const [flavorsCount, setFlavorsCount] = useState([flavorId()])
+  const [allFlavors, setAllFlavors] = useState([{id: flavorsCount[0], name: '', price: 0}])
 
   const addNewFlavor = () => {
-    setFlavorsCount([...flavorsCount, (flavorsCount[flavorsCount.length -1] + 1)])
+    const id = flavorId()
+    const obj = {
+      id,
+      name: '',
+      price: 0,
+    }
+    setFlavorsCount([...flavorsCount, id])
+    setAllFlavors([...allFlavors, obj])
   }
 
-  const exportFlavor = () => {
-    const obj = {
-      name,
-      price,
+  const exportFlavor = (flavors) => {
+    if (typeof flavors !== 'undefined') {
+      console.log(flavors)
+      return inputFunction(flavors)
     }
-    inputFunction(obj)
+    inputFunction(allFlavors)
+  }
+
+  const editFlavorObj = ({id, key, value}) =>{
+    const copy = [...allFlavors]
+
+    const findObj = copy.find((current) => current.id === id)
+    const indexObj = copy.indexOf(findObj)
+    copy[indexObj][key] = value
+    setAllFlavors(copy)
+    exportFlavor(copy)
   }
 
   const getFlavorName = (event) => {
-    setName(event.currentTarget.value)
-    exportFlavor()
+    const currentObjId = parseInt(event.currentTarget.id.split('-')[1])
+    const value = event.currentTarget.value
+
+    const obj = {
+      id: currentObjId,
+      key: 'name',
+      value
+    }
+
+    editFlavorObj(obj)
   }
 
   const getFlavorPrice = (event) => {
-    setPrice(event.currentTarget.value)
-    exportFlavor()
+    const currentObjId = parseInt(event.currentTarget.id.split('-')[1])
+    const value = event.currentTarget.value
+
+    const obj = {
+      id: currentObjId,
+      key: 'price',
+      value
+    }
+
+    editFlavorObj(obj)
   }
 
+  const removeFlavor = (event) => {
+    if (flavorsCount.length === 1) {
+      return alert('[ERRO] O produto deve conter ao menos um sabor!')
+    }
+
+    const currentObjId = parseInt(event.currentTarget.id)
+
+    const copyFlavors = [...allFlavors]
+
+    const findObj = copyFlavors.find((current) => current.id === currentObjId)
+    const indexObj = copyFlavors.indexOf(findObj)
+
+    copyFlavors.splice(indexObj, 1)
+
+    setAllFlavors(copyFlavors)
+    exportFlavor(copyFlavors)
+
+
+    const copyCount = flavorsCount
+    const findCounter = copyCount.find((current) => current === currentObjId)
+    const indexCounter = copyCount.indexOf(findCounter)
+    copyCount.splice(indexCounter, 1)
+
+    setFlavorsCount(copyCount)
+  }
+
+  const renderInputs = (quantity) => {
+    return quantity.map((current) => {
+      return (
+        <tr key={current} datatype={current}>
+          <td>
+            <input
+              type='text'
+              placeholder='Sabor'
+              id={`flavorName-${current}`}
+              onChange={getFlavorName}
+            />
+          </td>
+          <td className='inputPrice'>
+            <span>R$</span>
+            <input
+              type='number'
+              placeholder='0,00'
+              id={`flavorPrice-${current}`}
+              onChange={getFlavorPrice}
+            />
+            <BsX
+              id={current}
+              onClick={removeFlavor}
+            />
+          </td>
+        </tr>
+      )
+    })
+  }
 
   return (
     <div>
@@ -41,22 +132,13 @@ const FlavorsTable = ({inputFunction}) => {
           </tr>
         </thead>
         <tbody>
-        {flavorsCount.map((current) => {
-          return (
-            <tr key={current}>
-              <td>
-                <input type='text' placeholder='Sabor' onChange={getFlavorName}/>
-              </td>
-              <td className='inputPrice'>
-                <span>R$</span>
-                <input type='number' placeholder='0,00' onChange={getFlavorPrice}/>
-              </td>
-            </tr>
-          )
-        })}
+        {renderInputs(flavorsCount)}
         </tbody>
       </table>
-      <AiFillPlusSquare className='addNewFlavorButton' onClick={addNewFlavor}/>
+      <AiFillPlusSquare
+        className='addNewFlavorButton'
+        onClick={addNewFlavor}
+      />
     </div>
   )
 }
