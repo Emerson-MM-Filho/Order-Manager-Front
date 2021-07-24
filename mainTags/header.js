df8b88f3a662118df12041dd73ad913201448736
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-07-24 18:34:51 -0400

    delete files from older version

--- mainTags/header.js
+++
@@ -1,7 +0,0 @@
-const createHeader = () => {
-  const body = document.querySelector('body')
-  const header = document.createElement('header')
-  
-  header.classList.toggle('mainHeader')
-  body.appendChild(header)
-}
--- mainTags/main.js
+++
@@ -1,7 +0,0 @@
-const createMainContent = () => {
-    const body = document.querySelector('body')
-    const main = document.createElement('main')
-    
-    main.classList.toggle('mainContent')
-    body.appendChild(main)
-}
