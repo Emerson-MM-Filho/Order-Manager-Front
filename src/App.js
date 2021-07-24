d815c63215f1166ab52fc67a963edac798923ec5
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-07-24 18:57:36 -0400

    remove components

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
 import './App.css'
-import SideBarMenu from './components/sideBar/sideBarMenu'
-import NewOrder from './components/newOrder/newOrder'
-import Products from "./components/products/products";
-import Configs from "./components/configs/configs";
-import Clients from "./components/clients/clients";
-import AllOrders from "./components/allOrders/allOrders";
-import Alerts from "./components/alerts/alerts";
-
-import { useState } from "react"
-import MainSection from "./components/ui/mainSection";
 
 function App() {
-  const [sectionToShow, setSectionToShow] = useState('newOrder')
-  const renderSection = selectedSection => setSectionToShow(selectedSection)
+
 
   return (
     <main>
-      <aside id="sideBarMenu">
-        <SideBarMenu selected={renderSection}/>
-      </aside>
-      <MainSection>
-        {sectionToShow === 'newOrder' && <NewOrder/>}
-        {sectionToShow === 'allOrder' && <AllOrders/>}
-        {sectionToShow === 'clients' && <Clients/>}
-        {sectionToShow === 'products' && <Products/>}
-        {sectionToShow === 'configs' && <Configs/>}
-        {sectionToShow === 'alerts' && <Alerts/>}
-      </MainSection>
+
     </main>
   )
 }
