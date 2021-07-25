defd9191b2ad914dff80dfbb053ae035f3e7f6b1
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-07-24 20:15:03 -0400

    create route to home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom'
+
+import Home from './pages/Home'
 import './App.css'
 
 function App() {
 
 
   return (
-    <main>
-
-    </main>
+    <Switch>
+      <Route exact path='/' component={ Home } />
+    </Switch>
   )
 }
 
