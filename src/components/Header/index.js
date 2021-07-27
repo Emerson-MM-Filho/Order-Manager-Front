6a82c54ae3254dd849475fa14784aceb64ec501b
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-07-26 21:05:52 -0400

    create header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './style.scss'
+
+function HeaderComponent() {
+  return (
+    <header className='header-component'>
+      <div className='header-top-side'>
+        <p>oi</p>
+      </div>
+      <div className='header-bottom-side'>
+        <p>oi</p>
+      </div>
+    </header>
+  )
+}
+
+export default HeaderComponent
