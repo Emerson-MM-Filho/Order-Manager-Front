77846eae1238fcd3e914cc51e5e5137de2bd5e97
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-08-10 19:29:05 -0400

    create new order page

---
+++ src/pages/NewOrder.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HeaderComponent from '../components/Header'
+
+function NewOrder() {
+  return (
+    <div data-testid='new-order-page'>
+      <HeaderComponent />
+    </div>
+  )
+}
+
+export default NewOrder
