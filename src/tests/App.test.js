4caf844e5edaf08cc4afa247cdf966ff863272d2
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-08-10 19:25:08 -0400

    update data test id

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -5,13 +5,13 @@ import RenderWithRouter from './helpers/RenderWithRouter'
 import App from '../App'
 
 describe('Test the routes', () => {
-  test('Check if the "/" route renders the home page', () => {
+  test('Check if the "/" route renders the all orders page', () => {
     const { history } = RenderWithRouter(<App />)
     const { pathname } = history.location
 
     expect(pathname).toBe('/')
 
-    const homeComponent = screen.getByTestId('homeComponent')
+    const homeComponent = screen.getByTestId('all-orders-page')
 
     expect.anything(homeComponent);
   })
