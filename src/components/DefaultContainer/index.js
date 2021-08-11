a58ce1bfb3b74217eb1cad2636d1a5170ec1ca53
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-08-10 22:20:19 -0400

    create default container

---
+++ src/components/DefaultContainer/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.scss'
+
+function OrdersFilter({ children }) {
+  return (
+    <div className='default-container'>
+      { children }
+    </div>
+  )
+}
+
+export default OrdersFilter
