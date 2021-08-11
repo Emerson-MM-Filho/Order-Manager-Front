2d863d115b4b87cd4c5ea416d4204667d0f379a0
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-08-11 18:36:49 -0400

    create table row component to show all orders

--- src/components/AllOrdersTable/index.js
+++ src/components/AllOrdersTable/index.js
@@ -1,14 +1,42 @@
 import React from 'react'
 import './style.scss'
 
+import { plusIcon } from '../../icons'
 import DefaultContainer from '../DefaultContainer'
 import SearchBar from '../SearchBar'
+import DefaultButton from '../DefaultButton'
+import TableDataRow from '../TableDataRow'
+
+import { mockOrders } from '../../mockDataBase'
 
 function AllOrdersTable() {
   return (
-    <DefaultContainer>
+    <DefaultContainer addClass='all-orders'>
       <div className='all-orders-table-container'>
-        <SearchBar theme='gray'/>
+        <div className='all-orders-table-container-header'>
+          <SearchBar theme='gray'/>
+          <DefaultButton icon={plusIcon} text='Novo Pedido' />
+        </div>
+        <div className='all-orders-table'>
+          <table>
+            <thead>
+              <tr>
+                <th>Concluir</th>
+                <th>Cód. Pedido</th>
+                <th>Cliente</th>
+                <th>Data</th>
+                <th>Total</th>
+                <th>Método Pagamento</th>
+                <th>Status Pagamento</th>
+                <th>Método Envio</th>
+                <th>Ações</th>
+              </tr>
+            </thead>
+            <tbody>
+              {mockOrders.map((current) => <TableDataRow data={current} key={current.id}/>)}
+            </tbody>
+          </table>
+        </div>
       </div>
     </DefaultContainer>
   )
