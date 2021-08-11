b2d1e768077bf1934955ed98d4fd1e50fea3c0f8
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-08-11 16:00:56 -0400

    add dropdown and state to control render

--- src/components/NavSelectButton/index.js
+++ src/components/NavSelectButton/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.scss'
 import { selectArrowIcon } from '../../icons'
+import Dropdown from '../DropDown';
 
-function NavSelectButton({ icon, text }) {
+function NavSelectButton({ icon, text, options }) {
+  const [open, setOpen] = useState(false)
+
   return (
-    <div className="nav-select">
-      <span className="nav-select-icon">{ icon }</span>
-      <p className="nav-select-text">{ text }</p>
-      <span className="nav-select-arrow">{ selectArrowIcon }</span>
-    </div>
+    <nav>
+      <div className="nav-select" onClick={() => setOpen(!open)}>
+        <span className="nav-select-icon">{ icon }</span>
+        <p className="nav-select-text">{ text }</p>
+        <span className="nav-select-arrow">{ selectArrowIcon }</span>
+      </div>
+      {open && <Dropdown options={options}/>}
+    </nav>
   )
 }
 
