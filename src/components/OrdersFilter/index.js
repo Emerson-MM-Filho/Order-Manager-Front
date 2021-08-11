477947fa4255dabe3133762efe2f07155aa349a9
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-08-10 22:21:26 -0400

    create basic orders filter component

---
+++ src/components/OrdersFilter/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './style.scss'
+
+import DefaultContainer from '../DefaultContainer'
+
+function OrdersFilter() {
+  return (
+    <DefaultContainer>
+      <div className='orders-filter-container'>
+        <h2>Filtros</h2>
+      </div>
+    </DefaultContainer>
+  )
+}
+
+export default OrdersFilter
