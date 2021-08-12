55a86a56d26c5e6e520cb8ffb245c617cc6690ff
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-08-12 14:59:57 -0400

    create tests

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { screen } from '@testing-library/react'
+import { screen, fireEvent } from '@testing-library/react'
 import RenderWithRouter from './helpers/RenderWithRouter'
 
-import Header from '../components/Header'
+import App from '../App'
 
-describe('Test the Header component', () => {
+describe('Check the Header component', () => {
   beforeEach(() => {
-    RenderWithRouter(<Header />)
+    RenderWithRouter(<App />)
   })
   
   test('Check if the component is rendered', () => {
@@ -14,13 +14,46 @@ describe('Test the Header component', () => {
     expect.anything(header);
   })
 
-  test('Check if the logo is rendered', () => {
+  test('Check logo', () => {
     const logo = screen.getByTestId('logo')
     expect.anything(logo);
   })
 
-  test('Check if the search bar is rendered', () => {
+  test('Check search bar', () => {
     const searchBar = screen.getByTestId('search-bar')
     expect.anything(searchBar);
+  })
+  
+  test('Check profile button', () => {
+    const profileButton = screen.getAllByTestId('profile-button')
+    expect.anything(profileButton)
+  })
+
+  test('Check navigation menu', () => {
+    const nav = screen.getAllByTestId('nav-dropdown-button')
+    expect.anything(nav)
+  })
+
+  test('Check dropdown open', () => {
+    const nav = screen.getAllByTestId('nav-dropdown-button')
+    fireEvent.click(nav[0])
+    const dropdown = screen.getAllByTestId('nav-dropdown-open-container')
+    expect.anything(dropdown)
+  })
+})
+
+describe('Check the redirect', () => {
+  test('Check dropdown redirect', () => {
+    const { history } = RenderWithRouter(<App />);
+    
+    const nav = screen.getAllByTestId('nav-dropdown-button')
+    fireEvent.click(nav[0])
+
+    const link = screen.getAllByTestId('nav-dropdown-redirect-link')
+    fireEvent.click(link[1])
+    
+    const { pathname } = history.location;
+
+    expect(pathname).toBe('/new-order')
   })
 })
