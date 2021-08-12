6ca002d5156095e109ef182e35c3344b556f9f57
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-08-11 23:32:49 -0400

    rename component

--- src/pages/AllOrders/AllOrders.js
+++ src/pages/AllOrders/AllOrders.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './style.scss'
 
-import HeaderComponent from '../../components/Header'
+import Header from '../../components/Header'
 import OrdersFilter from '../../components/OrdersFilter'
 import AllOrdersTable from '../../components/AllOrdersTable'
 
 function allOrders() {
   return (
     <div data-testid='all-orders-page'>
-      <HeaderComponent />
+      <Header />
       <div className='all-orders-container'>
         <h1>Pedidos</h1>
         <div className='all-orders-page-content'>
