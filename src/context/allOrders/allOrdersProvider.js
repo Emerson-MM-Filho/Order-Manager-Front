d97029c87eef7eeda55a8d16ab016312e3e1ecee
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-08-14 22:47:27 -0400

    add order state

--- src/context/allOrders/allOrdersProvider.js
+++ src/context/allOrders/allOrdersProvider.js
@@ -3,10 +3,13 @@ import AllOrdersContext from './allOrdersContext'
 
 function Provider({children}) {
   const [modal, setModal] = useState(false)
+  const [order, setOrder] = useState({})
 
   const contextValue = {
     modal,
     setModal,
+    order,
+    setOrder,
   }
   
   return(
