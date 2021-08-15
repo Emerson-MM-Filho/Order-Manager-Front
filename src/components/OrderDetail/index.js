0d6bc0871a120bf2fec5c6282d251e93d44cd1f7
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-08-14 22:50:21 -0400

    create products table

--- src/components/OrderDetail/index.js
+++ src/components/OrderDetail/index.js
@@ -1,18 +1,26 @@
 import React, { useContext } from 'react'
 import './style.scss'
 
+import TableOrderDetails from '../TableOrderDetails'
 import AllOrdersContext from '../../context/allOrders/allOrdersContext'
 import DefaultModal from '../DefaultModal'; 
 import { closeIcon } from '../../icons'
 
-function OrderDetail() {
-  const { setModal } = useContext(AllOrdersContext)
 
+function OrderDetail() {
+  const { order, setModal } = useContext(AllOrdersContext)
+  
   return (
     <DefaultModal>
-      <div className='order-detail'>
-        <h2>Detalhes do Pedido</h2>
-        <button type='button' onClick={() => setModal(false)}>{closeIcon}</button>
+      <div className='order-detail-container'>
+        <div className='header'>
+          <h2>Detalhes do Pedido</h2>
+          <button type='button' onClick={() => setModal(false)}>{closeIcon}</button>
+        </div>
+        <div className='order-id'>
+          <p>ID Pedido: <span>{order.id}</span></p>
+        </div>
+        <TableOrderDetails products={order.products} />
       </div>
     </DefaultModal>
   )
