719ac9609550cc17e70b7e7762f6b991e852ab31
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-08-16 19:53:47 -0400

    create function to complete order

--- src/components/TableDataRowAllOrders/index.js
+++ src/components/TableDataRowAllOrders/index.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import './style.scss'
 
 import AllOrdersContext from '../../context/allOrders/allOrdersContext'
@@ -8,20 +8,24 @@ import DeliveryType from '../DeliveryType'
 
 import { eyeIcon, editIcon, trashIcon } from '../../icons'
 
-const total = (products) => {
-  return products.reduce((acc, current) => {
-    if(current.type === 'Wheight') {
-      return acc + ((current.quantity / 1000) * current.option.price)
-    }
-    return acc + (current.quantity * current.option.price)
-  }, 0).toFixed(2).replace('.', ',')
-}
 
 function TableDataRowAllOrders({data}) {
+  const [complete, setComplete] = useState(false)
+
+  const total = (products) => {
+    return products.reduce((acc, current) => {
+      if(current.type === 'Wheight') {
+        return acc + ((current.quantity / 1000) * current.option.price)
+      }
+      return acc + (current.quantity * current.option.price)
+    }, 0).toFixed(2).replace('.', ',')
+  }
+
   const { setModal, setOrder } = useContext(AllOrdersContext)
+
   return(
-    <tr className="all-orders-table-row">
-      <td><input type='checkbox' /></td>
+    <tr className={`all-orders-table-row ${complete && 'order-complete'}`}>
+      <td><input type='checkbox' onClick={() => setComplete(!complete)}/></td>
       <td>{data.id}</td>
       <td className='client-name'>{data.client.name}</td>
       <td>{data.delivery.date}</td>
