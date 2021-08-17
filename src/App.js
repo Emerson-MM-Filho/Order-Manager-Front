0b9667d34354e72629836c5182f0bd4c98bc8e1d
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-08-17 11:14:25 -0400

    remove provider

--- src/App.js
+++ src/App.js
@@ -2,17 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 import './App.scss'
 
-import Provider from './context/allOrders/allOrdersProvider'
 import AllOrders from './pages/AllOrders/AllOrders.js'
 import NewOrder from './pages/NewOrder/NewOrder'
 
 function App() {
   return (
     <Switch>
-      <Provider>
-        <Route exact path='/' component={ AllOrders } />
-        <Route exact path='/new-order' component={ NewOrder } />
-      </Provider>
+      <Route exact path='/' component={ AllOrders } />
+      <Route exact path='/new-order' component={ NewOrder } />
     </Switch>
   )
 }
