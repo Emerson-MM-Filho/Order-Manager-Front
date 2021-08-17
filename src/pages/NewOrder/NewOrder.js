7fd60b8642ac8ff814c2ce1e3b2ec2fd17a88e52
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-08-17 11:18:58 -0400

    add new order provider

--- src/pages/NewOrder/NewOrder.js
+++ src/pages/NewOrder/NewOrder.js
@@ -1,22 +1,25 @@
 import React from 'react'
 import './style.scss'
 
+import NewOrderProvider from '../../context/newOrder/newOrderProvider'
 import Header from '../../components/Header'
 import NewOrderTable from '../../components/NewOrderTable'
 import NewOrderForm from '../../components/NewOrderForm'
 
 function NewOrder() {
   return (
-    <div data-testid='new-order-page'>
-      <Header />
-      <div className='new-order-container'>
-        <h1>Novo Pedido</h1>
-        <div className='new-order-page-content'>
-          <NewOrderTable />
-          <NewOrderForm />
+    <NewOrderProvider>
+      <div data-testid='new-order-page'>
+        <Header />
+        <div className='new-order-container'>
+          <h1>Novo Pedido</h1>
+          <div className='new-order-page-content'>
+            <NewOrderTable />
+            <NewOrderForm />
+          </div>
         </div>
       </div>
-    </div>
+    </NewOrderProvider>
   )
 }
 
