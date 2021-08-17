9089098d81123340b6ab84449948ecbe42818fab
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-08-17 11:18:45 -0400

    remove provider and order detail

--- src/pages/AllOrders/AllOrders.js
+++ src/pages/AllOrders/AllOrders.js
@@ -1,26 +1,25 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import './style.scss'
 
-import AllOrdersContext from '../../context/allOrders/allOrdersContext'
+import AllOrdersProvider from '../../context/allOrders/allOrdersProvider'
 import Header from '../../components/Header'
 import OrdersFilter from '../../components/OrdersFilter'
 import AllOrdersTable from '../../components/AllOrdersTable'
-import OrderDetail from '../../components/OrderDetail'
 
 function AllOrders() {
-  const { modal } = useContext(AllOrdersContext)
   return (
-    <div data-testid='all-orders-page'>
-      {modal && <OrderDetail />}
-      <Header />
-      <div className='all-orders-container'>
-        <h1>Pedidos</h1>
-        <div className='all-orders-page-content'>
-          <OrdersFilter />
-          <AllOrdersTable />
+    <AllOrdersProvider>
+      <div data-testid='all-orders-page'>
+        <Header />
+        <div className='all-orders-container'>
+          <h1>Pedidos</h1>
+          <div className='all-orders-page-content'>
+            <OrdersFilter />
+            <AllOrdersTable />
+          </div>
         </div>
       </div>
-    </div>
+    </AllOrdersProvider>
   )
 }
 
