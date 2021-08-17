6cccebffdbb552e37f895c51ef565bd9faee1ea6
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-08-16 20:33:47 -0400

    add function to redirect to new order page

--- src/components/AllOrdersTable/index.js
+++ src/components/AllOrdersTable/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './style.scss'
 
+import { useHistory } from 'react-router-dom'
 import { plusIcon } from '../../icons'
 import DefaultContainer from '../DefaultContainer'
 import SearchBar from '../SearchBar'
@@ -10,12 +11,14 @@ import TableDataRowAllOrders from '../TableDataRowAllOrders'
 import { mockOrders } from '../../mockDataBase'
 
 function AllOrdersTable() {
+  const history = useHistory()
+
   return (
     <DefaultContainer addClass='all-orders'>
       <div className='all-orders-table-container'>
         <div className='all-orders-table-container-header'>
           <SearchBar theme='gray'/>
-          <DefaultButton icon={plusIcon} text='Novo Pedido' />
+          <DefaultButton icon={plusIcon} text='Novo Pedido' handleClick={() => history.push('/new-order')}/>
         </div>
         <div className='all-orders-table'>
           <table>
