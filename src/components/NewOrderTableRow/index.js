242d37f32538676eddfcdf9021c9db5437ed953c
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-09-05 19:04:46 -0400

    create button to remove product from new order

--- src/components/NewOrderTableRow/index.js
+++ src/components/NewOrderTableRow/index.js
@@ -3,9 +3,13 @@ import './style.scss'
 
 import SquareImage from '../SquareImage'
 import InputNumber from '../InputNumber'
+import { trashIcon } from '../../icons'
 
-function NewOrderTableRow({product}) {
-  return (
+function NewOrderTableRow({product, handleDelete}) {
+  const handleClick = () => {
+    handleDelete(product)
+  }
+   return (
     <tr key={product.options.id} className='new-order-product-row'>
       <td><SquareImage image={product.options.image} /></td>
       <td>
@@ -22,8 +26,13 @@ function NewOrderTableRow({product}) {
         </div>
       </td>
       <td>R$ {product.options.price.toFixed(2).replace('.',',')}</td>
-      <td><InputNumber/></td>
+      <td><InputNumber quantity={product.quantity} /></td>
       <td>R$ {product.options.price.toFixed(2).replace('.',',')}</td>
+      <td>
+        <button onClick={handleClick}>
+          {trashIcon}
+        </button>
+        </td>
     </tr>
   )
 }
