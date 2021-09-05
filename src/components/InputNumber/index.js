4b92ebca7bb4d71109bd0844db4e78dff767e7fe
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-09-05 18:59:21 -0400

    create function to increment and decrement number

--- src/components/InputNumber/index.js
+++ src/components/InputNumber/index.js
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './style.scss'
 
 import { plusIcon, minusIcon } from '../../icons'
 
-function InputNumber() {
+function InputNumber({quantity}) {
+  const [previousQuantity, setPreviousQuantity] = useState(quantity)
+  useEffect(() => {
+    setPreviousQuantity(quantity)
+  }, [setPreviousQuantity, quantity])
+  const handleClick = ({currentTarget: {name, value}}) => {
+    switch (name) {
+      case 'plus':
+        return setPreviousQuantity(previousQuantity + 1)
+      case 'minus':
+        if ( previousQuantity > 1 ) return setPreviousQuantity(previousQuantity - 1)
+        break
+      case 'input':
+        return setPreviousQuantity(parseFloat(value))
+      default:
+        break
+    }
+  }
+
   return (
     <div className='input-number-component'>
-      <input type="number" />
+      <input type="number" value={previousQuantity} name='input' onChange={(event) => handleClick(event)}/>
       <div className='increment-buttons'>
-        <button type='button'>{plusIcon}</button>
-        <button type='button'>{minusIcon}</button>
+        <button type='button' name='plus' onClick={(event) => handleClick(event)}>{plusIcon}</button>
+        <button type='button' name='minus' onClick={(event) => handleClick(event)}>{minusIcon}</button>
       </div>
     </div>
   )
