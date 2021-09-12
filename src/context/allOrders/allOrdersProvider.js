68310692dc81519117daf70f36b741136abeadd7
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-09-12 16:13:46 -0400

    create state to save all orders and condition to overflow when modal is open

--- src/context/allOrders/allOrdersProvider.js
+++ src/context/allOrders/allOrdersProvider.js
@@ -1,15 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import AllOrdersContext from './allOrdersContext'
 
+import { mockOrders } from '../../mockDataBase'
+
 function AllOrdersProvider({children}) {
+
   const [modal, setModal] = useState(false)
   const [order, setOrder] = useState({})
+  const [allOrders, setAllOrders] = useState(mockOrders)
+
+  useEffect(() => modal === true ? document.body.style.overflow = 'hidden' : document.body.style.overflow = 'unset' , [modal])
 
   const contextValue = {
     modal,
     setModal,
     order,
     setOrder,
+    allOrders,
+    setAllOrders,
   }
   
   return(
