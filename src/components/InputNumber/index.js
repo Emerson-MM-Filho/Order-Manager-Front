38615f6c0bc08c5ce62895ee462f7752dcfc7d85
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-09-12 16:17:56 -0400

    add function to update quantity

--- src/components/InputNumber/index.js
+++ src/components/InputNumber/index.js
@@ -3,7 +3,7 @@ import './style.scss'
 
 import { plusIcon, minusIcon } from '../../icons'
 
-function InputNumber({quantity}) {
+function InputNumber({quantity, func}) {
   const [previousQuantity, setPreviousQuantity] = useState(quantity)
   useEffect(() => {
     setPreviousQuantity(quantity)
@@ -11,11 +11,16 @@ function InputNumber({quantity}) {
   const handleClick = ({currentTarget: {name, value}}) => {
     switch (name) {
       case 'plus':
+        func(previousQuantity + 1)
         return setPreviousQuantity(previousQuantity + 1)
       case 'minus':
-        if ( previousQuantity > 1 ) return setPreviousQuantity(previousQuantity - 1)
+        if ( previousQuantity > 1 ) {
+          func(previousQuantity - 1)
+          return setPreviousQuantity(previousQuantity - 1)
+        }
         break
       case 'input':
+        func(parseFloat(value))
         return setPreviousQuantity(parseFloat(value))
       default:
         break
@@ -24,7 +29,7 @@ function InputNumber({quantity}) {
 
   return (
     <div className='input-number-component'>
-      <input type="number" value={previousQuantity} name='input' onChange={(event) => handleClick(event)}/>
+      <input type="number" min="1" value={previousQuantity} name='input' onChange={(event) => handleClick(event)}/>
       <div className='increment-buttons'>
         <button type='button' name='plus' onClick={(event) => handleClick(event)}>{plusIcon}</button>
         <button type='button' name='minus' onClick={(event) => handleClick(event)}>{minusIcon}</button>
