38f908fadea5673057479d4082457d78d5e43b50
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-09-12 16:11:41 -0400

    rename

---
+++ src/pages/NewAndEditOrder/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './style.scss'
+
+import NewOrderProvider from '../../context/newOrder/newOrderProvider'
+import Header from '../../components/Header'
+import NewOrderTable from '../../components/NewOrderTable'
+import NewOrderForm from '../../components/NewOrderForm'
+import ContentPageTitle from '../../components/ContentPageTitle'
+
+function NewAndEditOrder() {
+  return (
+    <NewOrderProvider>
+      <div data-testid='new-order-page'>
+        <Header />
+        <div className='new-order-container'>
+          <ContentPageTitle title='Novo Pedido'/>
+          <div className='new-order-page-content'>
+            <NewOrderTable />
+            <NewOrderForm />
+          </div>
+        </div>
+      </div>
+    </NewOrderProvider>
+  )
+}
+
+export default NewAndEditOrder
