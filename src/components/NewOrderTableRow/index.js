48acd6e0518aa0df5113cc02c8d54629e447c7ea
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-09-12 16:16:32 -0400

    create function to increment quantity in context and change generic button to component

--- src/components/NewOrderTableRow/index.js
+++ src/components/NewOrderTableRow/index.js
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './style.scss'
 
+import ButtonIcon from '../ButtonIcon'
 import SquareImage from '../SquareImage'
 import InputNumber from '../InputNumber'
 import { trashIcon } from '../../icons'
 
+import newOrderContext from '../../context/newOrder/newOrderContext'
+
 function NewOrderTableRow({product, handleDelete}) {
+  const { setProducts, products } = useContext(newOrderContext)
+
   const handleClick = () => {
     handleDelete(product)
+  }
+
+  const handleQuantity = (newQuantity) => {
+    const incrementedProduct = {
+      ...product,
+      quantity: newQuantity,
+    }
+    const newProducts = []
+    products.forEach(current => {
+      if (current.options.id === incrementedProduct.options.id && current.id === incrementedProduct.id) return newProducts.push(incrementedProduct)
+      return newProducts.push(current)
+    })
+    setProducts(newProducts)
   }
    return (
     <tr key={product.options.id} className='new-order-product-row'>
@@ -26,13 +44,11 @@ function NewOrderTableRow({product, handleDelete}) {
         </div>
       </td>
       <td>R$ {product.options.price.toFixed(2).replace('.',',')}</td>
-      <td><InputNumber quantity={product.quantity} /></td>
-      <td>R$ {product.options.price.toFixed(2).replace('.',',')}</td>
+      <td><InputNumber quantity={product.quantity} func={handleQuantity} /></td>
+      <td>R$ {(product.options.price * product.quantity).toFixed(2).replace('.',',')}</td>
       <td>
-        <button onClick={handleClick}>
-          {trashIcon}
-        </button>
-        </td>
+        <ButtonIcon onClick={handleClick} icon={trashIcon} />
+      </td>
     </tr>
   )
 }
