011b024a546001df7524567eb11ac5c68145046e
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-09-12 16:19:52 -0400

    create routes to products

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 import './App.scss'
 
-import AllOrders from './pages/AllOrders/AllOrders.js'
-import NewOrder from './pages/NewOrder/NewOrder'
+import AllOrders from './pages/AllOrders/'
+import NewAndEditOrder from './pages/NewAndEditOrder'
+import AllProducts from './pages/AllProducts'
 
 function App() {
   return (
     <Switch>
       <Route exact path='/' component={ AllOrders } />
-      <Route exact path='/new-order' component={ NewOrder } />
+      <Route exact path='/new-order' component={ NewAndEditOrder } />
+      <Route exact path='/order-edit/:id' component={ NewAndEditOrder } />
+      <Route exact path='/products' component={ AllProducts } />
     </Switch>
   )
 }
