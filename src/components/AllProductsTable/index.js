e1a37e4b5a0daade76e63d624e134b462c543e1a
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-09-24 10:27:39 -0400

    remove new product button

--- src/components/AllProductsTable/index.js
+++ src/components/AllProductsTable/index.js
@@ -1,24 +1,19 @@
 import React, { useContext } from 'react'
 import './style.scss'
 
-import { useHistory } from 'react-router-dom'
-import { plusIcon } from '../../icons'
 import DefaultContainer from '../DefaultContainer'
 import SearchBar from '../SearchBar'
-import DefaultButton from '../DefaultButton'
 import AllProductsContext from '../../context/allProducts/allProductsContext'
 import TableDataRowAllProducts from '../TableDataRowAllProducts'
 
 function AllProductsTable() {
   const { products } = useContext(AllProductsContext)
-  const history = useHistory()
 
   return (
     <DefaultContainer addClass='all-products'>
       <div className='all-products-table-container'>
         <div className='all-products-table-container-header'>
           <SearchBar theme='gray'/>
-          <DefaultButton icon={plusIcon} text='Novo Produto' handleClick={() => history.push('/new-product')}/>
         </div>
         <div className='all-products-table'>
           <table>
