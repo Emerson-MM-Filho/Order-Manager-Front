82f07a32906e2bf7a044d60442c0e943fe8c3e56
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-09-24 10:28:28 -0400

    add function in button adn dinamic text

--- src/components/ContentPageTitle/index.js
+++ src/components/ContentPageTitle/index.js
@@ -4,11 +4,11 @@ import DefaultButton from '../../components/DefaultButton'
 
 import { plusIcon } from '../../icons'
 
-function ContentPageTitle({title}) {
+function ContentPageTitle({title, buttonText, handleClick}) {
   return (
     <div className='content-page-title'>
       <h1>{title}</h1>
-      <DefaultButton icon={plusIcon} text='Salvar Pedido'/>
+      <DefaultButton icon={plusIcon} text={buttonText} handleClick={handleClick}/>
     </div>
   )
 }
