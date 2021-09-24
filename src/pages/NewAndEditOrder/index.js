c7222ab3a4fb6e4013c1cb0f47fac2c256bb2e27
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-09-24 10:30:49 -0400

    replaces page title with component with button

--- src/pages/NewAndEditOrder/index.js
+++ src/pages/NewAndEditOrder/index.js
@@ -13,7 +13,11 @@ function NewAndEditOrder() {
       <div data-testid='new-order-page'>
         <Header />
         <div className='new-order-container'>
-          <ContentPageTitle title='Novo Pedido'/>
+          <ContentPageTitle
+            title='Novo Pedido'
+            buttonText='Salvar Pedido'
+            handleClick={() => console.log('Pedido Salvo')}
+          />
           <div className='new-order-page-content'>
             <NewOrderTable />
             <NewOrderForm />
