959a4b1d4e8db221a49911cba6087e33314cbe21
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-09-24 10:30:29 -0400

    replaces page title with component with button

--- src/pages/AllOrders/index.js
+++ src/pages/AllOrders/index.js
@@ -1,18 +1,25 @@
 import React from 'react'
 import './style.scss'
 
+import { useHistory } from 'react-router-dom'
 import AllOrdersProvider from '../../context/allOrders/allOrdersProvider'
 import Header from '../../components/Header'
+import ContentPageTitle from '../../components/ContentPageTitle'
 import OrdersFilter from '../../components/OrdersFilter'
 import AllOrdersTable from '../../components/AllOrdersTable'
 
 function AllOrders() {
+  const history = useHistory()
   return (
     <AllOrdersProvider>
       <div data-testid='all-orders-page'>
         <Header />
         <div className='all-orders-container'>
-          <h1>Pedidos</h1>
+          <ContentPageTitle
+            title='Todos os Pedidos'
+            buttonText='Novo Pedido'
+            handleClick={() => history.push('/new-order')}
+          />
           <div className='all-orders-page-content'>
             <OrdersFilter />
             <AllOrdersTable />
