3cbda02f40d314de3b37ac57068f0c014ab5e228
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-09-24 10:30:37 -0400

    replaces page title with component with button

--- src/pages/AllProducts/index.js
+++ src/pages/AllProducts/index.js
@@ -1,19 +1,25 @@
 import React from 'react'
 import './style.scss'
 
+import { useHistory } from 'react-router-dom'
 import Header from '../../components/Header'
 import ProductsFilter from '../../components/ProductsFIlter'
 import AllProductsTable from '../../components/AllProductsTable'
-
+import ContentPageTitle from '../../components/ContentPageTitle'
 import AllProductsProvider from '../../context/allProducts/allProductsProvider'
 
 function AllProducts() {
+  const history = useHistory()
   return (
     <AllProductsProvider>
       <div data-testid='all-products-page'>
         <Header />
         <div className='all-products-container'>
-          <h1>Produtos</h1>
+          <ContentPageTitle
+            title='Todos os Produtos'
+            buttonText='Novo Produto'
+            handleClick={() => history.push('/new-product')}
+          />
           <div className='all-products-page-content'>
             <ProductsFilter />
             <AllProductsTable />
