d81554620f6ddcfa0496b1418899a8daec742a9f
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-09-24 11:05:12 -0400

    create new product options form component

---
+++ src/components/NewProductOptionsForm/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import DefaultContainer from '../DefaultContainer'
+
+function NewProductOptionsForm() {
+  return (
+    <DefaultContainer>
+      oi
+    </DefaultContainer>
+  )
+}
+
+export default NewProductOptionsForm
