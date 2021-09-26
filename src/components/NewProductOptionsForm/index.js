7e8b1d4a889ecbbc8d50963fff9744f052ef5184
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-09-26 19:44:13 -0400

    add table head and import row component

--- src/components/NewProductOptionsForm/index.js
+++ src/components/NewProductOptionsForm/index.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import './style.scss'
 
 import DefaultContainer from '../DefaultContainer'
+import NewProductOptionsTableRow from '../NewProductOptionsTableRow/index.js'
 
 function NewProductOptionsForm() {
   return (
-    <DefaultContainer>
-      oi
+    <DefaultContainer addClass='larger-container'>
+      <table>
+        <thead>
+          <tr>
+            <th>Imagem</th>
+            <th>Opção</th>
+            <th>Preço</th>
+          </tr>
+        </thead>
+        <tbody>
+          <NewProductOptionsTableRow />
+        </tbody>
+      </table>
     </DefaultContainer>
   )
 }
