027d43979194344d873019900a87c4b3c17796ea
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-09-30 00:05:42 -0400

    import new order reducer

--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import { allOrders } from './allOrders'
+import { newOrder } from './newOrder'
 
 export const reducer = combineReducers({
   allOrders,
+  newOrder,
 })
