b06d34f5553b98ed40356cb6315d7d868c61185c
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-09-30 13:51:20 -0400

    change default value to allOrder state to local storage itens

--- src/context/allOrders/allOrdersProvider.js
+++ src/context/allOrders/allOrdersProvider.js
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import AllOrdersContext from './allOrdersContext'
 
-import { mockOrders } from '../../mockDataBase'
-
 function AllOrdersProvider({children}) {
 
+  const allSavedsOrders = [JSON.parse(localStorage.getItem('allOrders'))] || []
   const [modal, setModal] = useState(false)
   const [order, setOrder] = useState({})
-  const [allOrders, setAllOrders] = useState(mockOrders)
+  const [allOrders, setAllOrders] = useState(allSavedsOrders)
 
   useEffect(() => modal === true ? document.body.style.overflow = 'hidden' : document.body.style.overflow = 'unset' , [modal])
 
