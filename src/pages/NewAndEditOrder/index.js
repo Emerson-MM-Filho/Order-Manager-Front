0a15aec87e0d56cd103b7656a14beee718c1401e
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-09-30 13:02:03 -0400

    dispatch create order

--- src/pages/NewAndEditOrder/index.js
+++ src/pages/NewAndEditOrder/index.js
@@ -1,5 +1,8 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import './style.scss'
+
+import { createOrderAction } from '../../redux/actions/newOrder'
 
 import NewOrderProvider from '../../context/newOrder/newOrderProvider'
 import Header from '../../components/Header'
@@ -8,6 +11,8 @@ import NewOrderForm from '../../components/NewOrderForm'
 import ContentPageTitle from '../../components/ContentPageTitle'
 
 function NewAndEditOrder() {
+  const dispatch = useDispatch()
+  const state = useSelector(state => state.newOrder)
   return (
     <NewOrderProvider>
       <div data-testid='new-order-page'>
@@ -16,7 +21,7 @@ function NewAndEditOrder() {
           <ContentPageTitle
             title='Novo Pedido'
             buttonText='Salvar Pedido'
-            handleClick={() => console.log('Pedido Salvo')}
+            handleClick={() => dispatch(createOrderAction(state))}
           />
           <div className='new-order-page-content'>
             <NewOrderTable />
