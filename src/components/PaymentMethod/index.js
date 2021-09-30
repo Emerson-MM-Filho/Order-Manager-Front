38471b1117613910fb487643df2f78236034f20c
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-09-30 14:35:20 -0400

    create function to show correct type

--- src/components/PaymentMethod/index.js
+++ src/components/PaymentMethod/index.js
@@ -4,10 +4,29 @@ import './style.scss'
 import { creditCardIcon, coinIcon } from '../../icons';
 
 function PaymentMethod({type, addClass}) {
+  const obj = (currentType) => {
+    switch (currentType) {
+      case 'creditCard':
+        return {icon: creditCardIcon, name: 'Cartão de Crédito'}
+
+      case 'debitCard':
+        return {icon: creditCardIcon, name: 'Cartão de Débito'}
+
+      case 'pix':
+        return {icon: coinIcon, name: 'Pix'}
+
+      case 'cash':
+      return {icon: coinIcon, name: 'Dinheiro'}
+  
+      default:
+        break
+    }
+  }
+
   return (
     <div className={`payment-method ${addClass}`}>
-      {type === 'Cartão' ? creditCardIcon : coinIcon}
-      {type}
+      {obj(type).icon}
+      {obj(type).name}
     </div>
   )
 }
