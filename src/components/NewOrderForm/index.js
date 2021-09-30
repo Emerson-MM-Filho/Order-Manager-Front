a1c7f2ae339a09eb181ad86ce785e77dd817d6bb
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-09-30 00:03:08 -0400

    move states to context and change function to use dispatch from redux

--- src/components/NewOrderForm/index.js
+++ src/components/NewOrderForm/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useContext } from 'react'
+import { useDispatch } from 'react-redux'
 import './style.scss'
 
 import newOrderContext from '../../context/newOrder/newOrderContext'
@@ -7,21 +8,14 @@ import LabelWithInput from '../LabelWithInput'
 import LabelWithSelect from '../LabelWithSelect'
 
 function NewOrderForm() {
-  const newDate = new Date()
+  const dispatch = useDispatch()
 
-  const day = newDate.getDate()
-  const month = newDate.getMonth()+1
-  const year = newDate.getFullYear()
-  const hour = newDate.getHours()
-  const minutes = newDate.getMinutes()
-
-  const currentDate = `${year}-${month < 10 ? `0${month}` : month}-${day}`
-  const currentHour = `${hour}:${minutes}`
-
-  const [paymentStatus, setPaymentStatus] = useState(false)
+  const {setDeliveryMethod, showDateTime, setShowDateTime, currentDate, currentHour, deliveryMethods, paymentsMethods} = useContext(newOrderContext)
+  
   const [shippingDate, setShippingDate] = useState(currentDate)
   const [shippingHour, setShippingHour] = useState(currentHour)
-  const {showAddress, setShowAddress, setDeliveryMethod, showDateTime, setShowDateTime, setClientForm, clientForm} = useContext(newOrderContext)
+  const [showAddress, setShowAddress] = useState(true)
+  const [paymentStatus, setPaymentStatus] = useState(false)
 
   const showAddressForm = ({target}) => {
     if(target.value === 'dispatch') {
@@ -32,62 +26,51 @@ function NewOrderForm() {
     setDeliveryMethod(target.value)
   }
 
-  const showDateTimeForm = ({target}) => {
-    setShowDateTime(!showDateTime)
-  }
-
-  const deliveryMethods = [
-    {
-      value: 'dispatch',
-      label: 'Entrega'
-    },
-    {
-      value: 'pickUp',
-      label: 'Retirada no local'
-    },
-  ]
-
-  const paymentsMethods = [
-    {
-      value: 'creditCard',
-      label: 'Cartão de Crédito'
-    },
-    {
-      value: 'debitCard',
-      label: 'Cartão de Débito'
-    },
-    {
-      value: 'pix',
-      label: 'Pix'
-    },
-    {
-      value: 'cash',
-      label: 'Dinheiro'
-    },
-  ]
-
   const handleChange = ({ target: { value, name } }) => {
-    setClientForm({
-      ...clientForm,
-      [name]: value,
-    })
+    dispatch(
+      {
+        type: 'RECIEVE_ORDER_FORM_INPUT',
+        name,
+        value
+      }
+    )
   }
 
   const handleClick = ({ target: { name } }) => {
     setPaymentStatus(!paymentStatus)
-    setClientForm({
-      ...clientForm,
-      [name]: !paymentStatus,
-    })
+    dispatch(
+      {
+        type: 'RECIEVE_ORDER_FORM_INPUT',
+        name,
+        value: !paymentStatus
+      }
+    )
   }
 
   return (
     <DefaultContainer addClass='new-order-form'>
       <h2>Detalhes da Entrega</h2>
       <div>
-        <LabelWithInput type='text' label='Nome' text='Insira o Nome' name='clientName' onChange={(event) => handleChange(event)}/>
-        <LabelWithInput type='text' label='Telefone' text='Insira o Telefone' name='clientPhone' onChange={(event) => handleChange(event)}/>
-        <LabelWithSelect options={paymentsMethods} text='Método de Pagamento' name='paymentMethod' onChange={(event) => handleChange(event)}/>
+        <LabelWithInput
+          type='text'
+          label='Nome'
+          text='Insira o Nome'
+          name='clientName'
+          onChange={(event) => handleChange(event)}
+        />
+        <LabelWithInput
+          type='text'
+          label='Telefone'
+          text='Insira o Telefone'
+          name='clientPhone'
+          onChange={(event) => handleChange(event)}
+        />
+        <LabelWithSelect
+          options={paymentsMethods}
+          text='Método de Pagamento'
+          name='paymentMethod'
+          onChange={(event) => handleChange(event)}
+        />
         <LabelWithInput
           type='checkbox'
           label='Este pedido está pago?'
@@ -105,19 +88,45 @@ function NewOrderForm() {
         />
         {showAddress && (
           <div>
-            <LabelWithInput type='text' label='Endereço' text='Insira o Endereço de Entrega' name='addressStreet' onChange={(event) => handleChange(event)}/>
+            <LabelWithInput
+              type='text'
+              label='Endereço'
+              text='Insira o Endereço de Entrega'
+              name='addressStreet'
+              onChange={(event) => handleChange(event)}
+            />
             <div className='inline-input'>
-              <LabelWithInput type='number' label='Número' text='Insira o Número' addClass='inline' name='addressNumber' onChange={(event) => handleChange(event)}/>
-              <LabelWithInput type='text' label='Bairro' text='Insira o Bairro' addClass='inline' name='addressDistrict' onChange={(event) => handleChange(event)}/>
+              <LabelWithInput 
+                type='number'
+                label='Número'
+                text='Insira o Número'
+                addClass='inline'
+                name='addressNumber'
+                onChange={(event) => handleChange(event)}
+              />
+              <LabelWithInput
+                type='text'
+                label='Bairro'
+                text='Insira o Bairro'
+                addClass='inline'
+                name='addressDistrict'
+                onChange={(event) => handleChange(event)}
+              />
             </div>
-            <LabelWithInput type='text' label='Complemento' text='Insira o Complemento' name='addressComplement' onChange={(event) => handleChange(event)}/>
+            <LabelWithInput
+              type='text'
+              label='Complemento'
+              text='Insira o Complemento'
+              name='addressComplement'
+              onChange={(event) => handleChange(event)}
+            />
           </div>
         )}
         <LabelWithInput
           type='checkbox'
           label='Este pedido é uma encomenda?'
           addClass='inline-label'
-          onClick={(event) => showDateTimeForm(event)}
+          onClick={() => setShowDateTime(!showDateTime)}
         />
         {showDateTime && (
           <div className='inline-input'>
@@ -148,7 +157,13 @@ function NewOrderForm() {
             />
           </div>
         )}
-        <LabelWithInput type='text' label='Anotação' text='Insira uma anotação ao pedido' name='note' onChange={(event) => handleChange(event)}/>
+        <LabelWithInput
+          type='text'
+          label='Anotação'
+          text='Insira uma anotação ao pedido'
+          name='note'
+          onChange={(event) => handleChange(event)}
+        />
       </div>
     </DefaultContainer>
   )
