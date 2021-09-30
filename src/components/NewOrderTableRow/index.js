7c9654cc89beacc1ad5bbd7be57a0b5d4693a032
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-09-30 14:34:53 -0400

    change options key to option

--- src/components/NewOrderTableRow/index.js
+++ src/components/NewOrderTableRow/index.js
@@ -22,14 +22,14 @@ function NewOrderTableRow({product, handleDelete}) {
     }
     const newProducts = []
     products.forEach(current => {
-      if (current.options.id === incrementedProduct.options.id && current.id === incrementedProduct.id) return newProducts.push(incrementedProduct)
+      if (current.option.id === incrementedProduct.option.id && current.id === incrementedProduct.id) return newProducts.push(incrementedProduct)
       return newProducts.push(current)
     })
     setProducts(newProducts)
   }
    return (
-    <tr key={product.options.id} className='new-order-product-row'>
-      <td><SquareImage image={product.options.image} /></td>
+    <tr key={product.option.id} className='new-order-product-row'>
+      <td><SquareImage image={product.option.image} /></td>
       <td>
         <div className='product-option'>
           <p>
@@ -38,14 +38,14 @@ function NewOrderTableRow({product, handleDelete}) {
           <p className='option-selected'>
             Opção:
             <span>
-              {product.options.optionName}
+              {product.option.optionName}
             </span>
           </p>
         </div>
       </td>
-      <td>R$ {product.options.price.toFixed(2).replace('.',',')}</td>
+      <td>R$ {product.option.price.toFixed(2).replace('.',',')}</td>
       <td><InputNumber quantity={product.quantity} func={handleQuantity} /></td>
-      <td>R$ {(product.options.price * product.quantity).toFixed(2).replace('.',',')}</td>
+      <td>R$ {(product.option.price * product.quantity).toFixed(2).replace('.',',')}</td>
       <td>
         <ButtonIcon onClick={handleClick} icon={trashIcon} />
       </td>
