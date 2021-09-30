435ff1776e2f854d7fed9c062558e01db799ff3f
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-09-30 13:02:26 -0400

    create actions to add products and crete order

--- src/redux/actions/newOrder.js
+++ src/redux/actions/newOrder.js
@@ -1,7 +1,17 @@
-import { RECIEVE_ORDER_FORM_INPUT } from './index'
+import { RECIEVE_ORDER_FORM_INPUT, CREATE_ORDER, ADD_PRODUCTS } from './index'
 
 export const recieveOrderFormInputAction = (name, value) => ({
   type: RECIEVE_ORDER_FORM_INPUT,
   name,
   value
+})
+
+export const addProductsAction = (products) => ({
+  type: ADD_PRODUCTS,
+  products,
+})
+
+export const createOrderAction = (inputs) => ({
+  type: CREATE_ORDER,
+  inputs,
 })
