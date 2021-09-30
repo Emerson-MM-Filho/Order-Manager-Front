042025f4c53132563b7276b2aa4005d8d7e7f236
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-09-30 00:03:45 -0400

    import const and states

--- src/context/newOrder/newOrderProvider.js
+++ src/context/newOrder/newOrderProvider.js
@@ -2,18 +2,55 @@ import React, { useState, useEffect } from 'react'
 import NewOrderContext from './newOrderContext'
 
 function NewOrderProvider({children}) {
-  const [showAddress, setShowAddress] = useState(true)
   const [deliveryMethod, setDeliveryMethod] = useState('dispatch')
   const [showDateTime, setShowDateTime] = useState(false)
   const [products, setProducts] = useState([])
   const [modal, setModal] = useState(false)
   const [clientForm, setClientForm] = useState({})
 
+  const newDate = new Date()
+  const day = newDate.getDate()
+  const month = newDate.getMonth()+1
+  const year = newDate.getFullYear()
+  const hour = newDate.getHours()
+  const minutes = newDate.getMinutes()
+
+  const currentDate = `${year}-${month < 10 ? `0${month}` : month}-${day}`
+  const currentHour = `${hour}:${minutes}`
+
+  const deliveryMethods = [
+    {
+      value: 'dispatch',
+      label: 'Entrega'
+    },
+    {
+      value: 'pickUp',
+      label: 'Retirada no local'
+    },
+  ]
+
+  const paymentsMethods = [
+    {
+      value: 'creditCard',
+      label: 'Cartão de Crédito'
+    },
+    {
+      value: 'debitCard',
+      label: 'Cartão de Débito'
+    },
+    {
+      value: 'pix',
+      label: 'Pix'
+    },
+    {
+      value: 'cash',
+      label: 'Dinheiro'
+    },
+  ]
+
   useEffect(() => modal === true ? document.body.style.overflow = 'hidden' : document.body.style.overflow = 'unset' , [modal])
 
   const contextValue = {
-    showAddress,
-    setShowAddress,
     deliveryMethod,
     setDeliveryMethod,
     showDateTime,
@@ -24,6 +61,10 @@ function NewOrderProvider({children}) {
     setModal,
     clientForm,
     setClientForm,
+    currentDate,
+    currentHour,
+    deliveryMethods,
+    paymentsMethods,
   }
   
   return(
