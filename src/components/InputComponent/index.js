ce5887f615e11013e41d298ab04b9da8d9987996
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-09-30 21:35:37 -0400

    transform component in a controlled component

--- src/components/InputComponent/index.js
+++ src/components/InputComponent/index.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.scss'
 
-function InputComponent({type, text, ...rest}) {
+function InputComponent({type, text, handleChange, inputValue, ...rest}) {
+  const [defaultValue, setDefaultValue] = useState(inputValue || '')
   return(
-    <input type={type} placeholder={text} className='default-input' {...rest}/>
+    <input
+      type={type}
+      placeholder={text}
+      className='default-input'
+      value={defaultValue}
+      onChange={(event) => {
+        handleChange(event)
+        setDefaultValue(event.target.value)
+      }}
+      {...rest}
+    />
   )
 }
 
